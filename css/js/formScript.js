8b9d9e220f192b459b2550ada1da4da08b2cff9b
FionanAnderson <30053520@live.nwrc.ac.uk>
2026-01-24 00:20:07 +0000

    Add files via upload

---
+++ css/js/formScript.js
@@ -0,0 +1,40 @@
+function myFormSubmits(event) {
+    event.preventDefault();
+
+    
+    downloadFormData();
+    thanks();
+}
+
+function downloadFormData() {
+    const form = document.getElementById('myForm');
+    const formData = new FormData(form);
+    let dataString = '';
+
+    for (let [key, value] of formData.entries()) {
+        dataString += `${key}: ${value}\n`;
+    }
+
+    
+    const blob = new Blob([dataString], {
+        type: 'text/plain'
+    });
+
+    const url = URL.createObjectURL(blob);
+
+  
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'form-data.txt';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    
+    URL.revokeObjectURL(url);
+}
+
+function thanks() {
+    
+    window.location.href = 'thankyou.html'; 
+}
